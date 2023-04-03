627746f13c20c511b9d62c3e589a2a8d81d7bc19
BHowell <bretthowellcrc@gmail.com>
2023-04-03 18:21:59 -0400

    added new lambda test3

--- backend/count_test.py
+++ backend/count_test.py
@@ -2,16 +2,18 @@ import json
 from unittest.mock import patch
 from count import lambda_handler
 
-@patch('count.table')
-def test_lambda_handler(mock_table):
-    mock_table.return_value.get_item.return_value = {'Item': {'count': 0}}
+@patch('boto3.resource')
+def test_lambda_handler(mock_resource):
+    mock_table = mock_resource().Table()
+    mock_table.get_item.return_value = {'Item': {'count': 0}}
+
     # Test POST method
     response = lambda_handler({'httpMethod': 'POST'}, {})
     assert response['statusCode'] == 200
     assert json.loads(response['body'])['message'] == 'New view count is 1'
 
     # Test GET method
-    mock_table.return_value.get_item.return_value = {'Item': {'count': 1}}
+    mock_table.get_item.return_value = {'Item': {'count': 1}}
     response = lambda_handler({'httpMethod': 'GET'}, {})
     assert response['statusCode'] == 200
     assert json.loads(response['body'])['count'] == '1'
