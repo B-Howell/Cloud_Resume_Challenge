db4caf0036e8c187b365b2aec75620f66aaea777
BHowell <bretthowellcrc@gmail.com>
2023-04-03 18:29:15 -0400

    added new lambda test4

--- backend/count_test.py
+++ backend/count_test.py
@@ -3,22 +3,13 @@ from unittest.mock import patch
 from count import lambda_handler
 
 @patch('boto3.resource')
-def test_lambda_handler(mock_resource):
-    mock_table = mock_resource().Table()
-    mock_table.get_item.return_value = {'Item': {'count': 0}}
+def test_lambda_handler(mock_table):
+    mock_table.return_value.get_item.return_value = {'Item': {'count': 0}}
+
+    # Test GET method
+    response = lambda_handler({'httpMethod': 'GET'}, {})
+    assert response['statusCode'] == 200
 
     # Test POST method
     response = lambda_handler({'httpMethod': 'POST'}, {})
     assert response['statusCode'] == 200
-    assert json.loads(response['body'])['message'] == 'New view count is 1'
-
-    # Test GET method
-    mock_table.get_item.return_value = {'Item': {'count': 1}}
-    response = lambda_handler({'httpMethod': 'GET'}, {})
-    assert response['statusCode'] == 200
-    assert json.loads(response['body'])['count'] == '1'
-
-    # Test invalid method
-    response = lambda_handler({'httpMethod': 'PUT'}, {})
-    assert response['statusCode'] == 405
-    assert json.loads(response['body'])['error'] == 'Method not allowed'
