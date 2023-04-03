960f42c1fdb1bd813a2dd229be51fde2a0957d58
BHowell <bretthowellcrc@gmail.com>
2023-04-03 17:55:30 -0400

    lambda test 14

---
+++ backend/count_test.py
@@ -0,0 +1,22 @@
+import json
+from unittest.mock import patch
+from lambda_function import lambda_handler
+
+@patch('lambda_function.table')
+def test_lambda_handler(mock_table):
+    mock_table.return_value.get_item.return_value = {'Item': {'count': 0}}
+    # Test POST method
+    response = lambda_handler({'httpMethod': 'POST'}, {})
+    assert response['statusCode'] == 200
+    assert json.loads(response['body'])['message'] == 'New view count is 1'
+
+    # Test GET method
+    mock_table.return_value.get_item.return_value = {'Item': {'count': 1}}
+    response = lambda_handler({'httpMethod': 'GET'}, {})
+    assert response['statusCode'] == 200
+    assert json.loads(response['body'])['count'] == '1'
+
+    # Test invalid method
+    response = lambda_handler({'httpMethod': 'PUT'}, {})
+    assert response['statusCode'] == 405
+    assert json.loads(response['body'])['error'] == 'Method not allowed'
